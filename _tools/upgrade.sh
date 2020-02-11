54f160ef9a1c119b2db80b3b8c1f300575047c7c
Oliver Kaufmann <okaufmann91@gmail.com>
2020-02-11 22:10:28 +0100

    dont install passport on upgrade

--- _tools/upgrade.sh
+++ _tools/upgrade.sh
@@ -10,5 +10,5 @@ php artisan migrate --seed
 php artisan firefly-iii:decrypt-all
 php artisan cache:clear
 php artisan firefly-iii:upgrade-database
-php artisan passport:install
+# php artisan passport:install
 php artisan cache:clear
