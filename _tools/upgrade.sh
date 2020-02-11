6d303519151b2e103f7aed250c11e02464eae651
Oliver Kaufmann <okaufmann91@gmail.com>
2020-02-11 22:29:15 +0100

    add update notice

--- _tools/upgrade.sh
+++ _tools/upgrade.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")/.."
 
-git pull
-rm -rf bootstrap/cache/*
-rm -rf vendor/
-composer install --no-scripts --no-dev
-composer install --no-dev
-php artisan migrate --seed
-php artisan firefly-iii:decrypt-all
-php artisan cache:clear
-php artisan firefly-iii:upgrade-database
-# php artisan passport:install
-php artisan cache:clear
+# use forge to deploy!
